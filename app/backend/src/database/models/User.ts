f4570feef468d863ff3ec4e99ae1e8daa2d9687a
angelicapedroso <angelicapedroso05@gmail.com>
2022-08-20 10:54:11 -0300

    refactor: user model

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,35 @@
+import { Model, STRING, INTEGER } from 'sequelize';
+import db from '.';
+
+export default class User extends Model {
+  id!: number;
+  username!: string;
+  role!: string;
+  email!: string;
+  password!: string;
+}
+
+User.init({
+  id: {
+    type: INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  username: {
+    type: STRING,
+    allowNull: false,
+  },
+  role: {
+    type: STRING,
+    allowNull: false,
+  },
+  email: {
+    type: STRING,
+    allowNull: false,
+  },
+  password: {
+    type: STRING,
+    allowNull: false,
+  },
+}, { sequelize: db, underscored: true });
